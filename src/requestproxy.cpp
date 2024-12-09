87d4fa92953c3a22b4d73f53f75005a5d7d0bb09
kamchatka-volcano <gorelyy@eelnet.org>
2024-12-09 23:11:44 +0500

    -updated hot_teacup, now http request has access to fastcgi params

--- src/requestproxy.cpp
+++ src/requestproxy.cpp
@@ -2,13 +2,19 @@
 #include <asyncgi/http/request_view.h>
 #include <asyncgi/types.h>
 #include <fcgi_responder/request.h>
+#include <unordered_map>
 
 namespace asyncgi::detail {
 
 RequestProxy::RequestProxy(const fcgi::Request& request)
     : fcgiRequest_{fastcgi::detail::RequestView{request}}
-    , httpRequest_{[this]
-                   {
+    , httpRequest_{
+              [this]
+              {
+                  auto fcgiParams = std::unordered_map<std::string_view, std::string_view>{};
+                  for (const auto& [key, value] : fcgiRequest_.params())
+                      fcgiParams.emplace(key, value);
+
                   return http::Request{http::RequestView{
                           fcgiRequest_.hasParam("REQUEST_METHOD") ? fcgiRequest_.param("REQUEST_METHOD")
                                                                   : std::string_view{},
@@ -20,7 +26,8 @@ RequestProxy::RequestProxy(const fcgi::Request& request)
                           fcgiRequest_.hasParam("HTTP_COOKIE") ? fcgiRequest_.param("HTTP_COOKIE") : std::string_view{},
                           fcgiRequest_.hasParam("CONTENT_TYPE") ? fcgiRequest_.param("CONTENT_TYPE")
                                                                 : std::string_view{},
-                          fcgiRequest_.stdIn()}};
+                          fcgiRequest_.stdIn(),
+                          std::move(fcgiParams)}};
               }}
 {
 }
